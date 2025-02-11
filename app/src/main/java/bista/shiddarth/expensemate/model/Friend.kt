ac9389ac500e4c162f85ce577d2391a5a3f8c31b
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2025-02-11 10:34:12 -0500

    add email back

--- app/src/main/java/bista/shiddarth/expensemate/model/Friend.kt
+++ app/src/main/java/bista/shiddarth/expensemate/model/Friend.kt
@@ -6,5 +6,6 @@ data class Friend(
     val id: UUID = UUID.randomUUID(),
     val firstName: String,
     val lastName: String,
+    val email: String,
     val expenses: MutableList<Expense> = mutableListOf()
 )
